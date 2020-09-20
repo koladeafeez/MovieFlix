a9a94bfc85f9acfe95b11df4458ae8ed67ce93d1
kolade Afeez <daisedalok@gmail.com>
2020-09-20 23:27:22 +0100

    build

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,29 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from './auth.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "./auth.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
   mouseoverLogin;
   userName;
-emailAddress;
-password;
-  constructor(private router: Router,
-    private authservice: AuthService) { }
+  emailAddress;
+  password;
+  constructor(private router: Router, private authservice: AuthService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  goHome(){
-    
-  }
-  login(formvalue){
+  goHome() {}
+  login(formvalue) {
     this.authservice.loginUser(formvalue.userName, formvalue.password);
-   this.router.navigate(['/movies']);
+    this.router.navigate(["/movies"]);
   }
 }
-
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,57 +1,93 @@
-import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
-import { Router} from '@angular/router'
-import { AuthService } from '../login/auth.service'
+import { Router } from "@angular/router";
+import { AuthService } from "../login/auth.service";
 
 @Component({
-  templateUrl: './user-profile.component.html',
-  styles: [`
-    em {float:right; color:#E05C65; padding-left: 10px;}
-    .error input {background-color:#E3C3C5;}
-    .error ::-webkit-input-placeholder { color: #999; }
-    .error ::-moz-placeholder { color: #999; }
-    .error :-moz-placeholder { color:#999; }
-    .error :ms-input-placeholder { color: #999; }
-  `]
+  templateUrl: "./user-profile.component.html",
+  styles: [
+    `
+      h1 {
+        color: #ffffff;
+      }
+      label {
+        color: #ffffff;
+      }
+      .profile-cover {
+        margin-left: 2rem;
+        margin-top: 3rem;
+      }
+      em {
+        float: right;
+        color: #e05c65;
+        padding-left: 10px;
+      }
+      .cancel {
+        background: #ffffff;
+        color: #000000;
+        margin-left: 0.5rem;
+      }
+      .error input {
+        background-color: #e3c3c5;
+      }
+      .error ::-webkit-input-placeholder {
+        color: #999;
+      }
+      .error ::-moz-placeholder {
+        color: #999;
+      }
+      .error :-moz-placeholder {
+        color: #999;
+      }
+      .error :ms-input-placeholder {
+        color: #999;
+      }
+    `,
+  ],
 })
-
 export class UserProfileComponent implements OnInit {
-  profileForm:FormGroup
-  private firstName:FormControl
-  private lastName:FormControl
+  profileForm: FormGroup;
+  private firstName: FormControl;
+  private lastName: FormControl;
 
-  constructor(private router:Router, private authService:AuthService) {
-
-  }
+  constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit() {
-    this.firstName = new FormControl(this.authService.currentUser.firstName, [Validators.required, Validators.pattern('[a-zA-Z].*')])
-    this.lastName = new FormControl(this.authService.currentUser.lastName, Validators.required)
+    this.firstName = new FormControl(this.authService.currentUser.firstName, [
+      Validators.required,
+      Validators.pattern("[a-zA-Z].*"),
+    ]);
+    this.lastName = new FormControl(
+      this.authService.currentUser.lastName,
+      Validators.required
+    );
 
     this.profileForm = new FormGroup({
       firstName: this.firstName,
-      lastName: this.lastName
-    })
+      lastName: this.lastName,
+    });
   }
 
   saveProfile(formValues) {
     if (this.profileForm.valid) {
-      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
-      this.router.navigate(['movies'])
+      this.authService.updateCurrentUser(
+        formValues.firstName,
+        formValues.lastName
+      );
+      this.router.navigate(["movies"]);
     }
   }
 
   validateFirstName() {
-    return this.firstName.valid || this.firstName.untouched
+    return this.firstName.valid || this.firstName.untouched;
   }
-  
+
   validateLastName() {
-    return this.lastName.valid || this.lastName.untouched
+    return this.lastName.valid || this.lastName.untouched;
   }
 
   cancel() {
-    this.router.navigate(['movies'])
+    this.router.navigate(["movies"]);
   }
-       
 }
